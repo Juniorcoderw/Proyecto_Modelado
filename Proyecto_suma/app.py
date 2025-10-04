267f4cd521b0a171c5a3b95901e30b7e050e4eda
Faelmatacholad <124718963+Faelmatacholad@users.noreply.github.com>
2025-10-03 22:44:52 -0500

    Update app.py

--- Proyecto_suma/app.py
+++ Proyecto_suma/app.py
@@ -1,18 +1,1 @@
-from flask import Flask, render_template, request
 
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    resultado = None
-    if request.method == "POST":
-        num1 = int(request.form["num1"])
-        num2 = int(request.form["num2"])
-        resultado = num1 + num2
-    return render_template("index.html", resultado=resultado)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-    HOLA 
